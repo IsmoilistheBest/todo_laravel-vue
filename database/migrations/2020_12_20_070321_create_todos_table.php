70438f2342b26bface4727df52762095f3992e55
476645 <rc@47RC.47RC>
2020-12-25 11:00:33 -0600

    make it better

--- database/migrations/2020_12_20_070321_create_todos_table.php
+++ database/migrations/2020_12_20_070321_create_todos_table.php
@@ -16,7 +16,7 @@ class CreateTodosTable extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->text('title');
+            $table->string('title');
             $table->boolean('completed')->default(0);
             $table->timestamps();
             $table->softDeletes();
